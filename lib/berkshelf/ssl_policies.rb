b51a4b66d740b1b2d78eb1dc4123ab767559d476
Eric Rouleau <xblitz@gmail.com>
2018-10-22 23:53:50 -0400

    Update ssl_policies.rb

--- lib/berkshelf/ssl_policies.rb
+++ lib/berkshelf/ssl_policies.rb
@@ -31,13 +31,9 @@ module Berkshelf
     end
 
     def set_custom_certs
-      Berkshelf.mutex_chdir.synchronize do
-        Dir.chdir(trusted_certs_dir) do
-          ::Dir.glob("{*.crt,*.pem}").each do |cert|
-            cert = OpenSSL::X509::Certificate.new(IO.read(cert))
-            add_trusted_cert(cert)
-          end
-        end
+      ::Dir.glob("#{trusted_certs_dir}/{*.crt,*.pem}").each do |cert|
+        cert = OpenSSL::X509::Certificate.new(IO.read(cert))
+        add_trusted_cert(cert)
       end
     end
   end
